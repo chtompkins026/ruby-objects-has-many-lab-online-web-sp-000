9bf05f5101f98439c4267289a9fb0611ca73c578
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-24 15:58:12 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,7 +15,7 @@ class Author
   end 
   
   def add_post_by_title(post_name)
-    song_name = Song.new(song_name)
+    post_name = Post.new(post_name)
     songs << song_name
     song_name.artist = self 
     @@song_count += 1 
