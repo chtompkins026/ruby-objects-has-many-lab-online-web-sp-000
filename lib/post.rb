1c085a8f243bcb554a32b02c0a4bf85552d24c4a
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-24 16:09:17 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,12 @@
 class Post
+  
   attr_accessor :title, :author
   @@all = []
  
   
   def initialize(title)
     @title = title
-    @@all << title
+    @@all << @title
   end
 
   def author_name
