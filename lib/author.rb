83a484a2a850f8f6a1dd5fb27a02e7c5c95969e4
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-24 16:05:30 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -17,9 +17,9 @@ class Author
 
   def add_post_by_title(title)
     post = Post.new(title)
-    l@posts << post
-    lpost.author = self
-    l@@post_count += 1
+    @posts << post
+    post.author = self
+    @@post_count += 1
   end
     
   def self.post_count
